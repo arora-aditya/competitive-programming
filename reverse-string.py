00652c2c1d63e717d37616fdf942e9c59155405e
Aditya Arora <arora-aditya@users.noreply.github.com>
2017-08-19 19:16:30 +0530

    Create reverse-string.py

---
+++ reverse-string.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        https://leetcode.com/problems/reverse-string/description/
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
