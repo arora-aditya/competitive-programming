003bf668b3617e12a96818275a9bf7ce7bcd4bf7
Aditya Arora <arora-aditya@users.noreply.github.com>
2017-08-24 13:25:36 +0530

    Add files via upload

---
+++ intToRoman.py
@@ -0,0 +1,12 @@
+def intToRoman(self, num):
+    """
+    https://leetcode.com/problems/integer-to-roman/description/
+    :type num: int
+    :rtype: str
+    """
+    M = ["", "M", "MM", "MMM"]
+    C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    
+    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10]
