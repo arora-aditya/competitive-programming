f94c9e6958e3ca241d94d3a6bc1bfbf553e60cd5
Aditya Arora <arora-aditya@users.noreply.github.com>
2017-10-12 16:43:53 -0400

    removed unnecessary print statement

--- binary-number-with-alternating-bits.py
+++ binary-number-with-alternating-bits.py
@@ -6,7 +6,6 @@ class Solution(object):
         https://leetcode.com/problems/binary-number-with-alternating-bits/description/
         """
         xBin = "{0:b}".format(n)
-        print(xBin)
         Value = xBin[0]
         for i in range(len(xBin)-1):
             if xBin[i] == xBin[i+1]:
