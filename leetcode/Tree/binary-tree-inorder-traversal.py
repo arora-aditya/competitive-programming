d50759689cfffc5d0d064fe275d82ec3c978e48f
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2018-02-09 14:01:02 -0500

    practice

---
+++ leetcode/Tree/binary-tree-inorder-traversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
---
+++ leetcode/Tree/closest-binary-search-tree-value.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def closestValue(self, root, target):
+        """
+        https://leetcode.com/problems/closest-binary-search-tree-value/description/
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
+        r = root.val
+        while root:
+            if abs(root.val - target) < abs(r - target):
+                r = root.val
+            root = root.left if target < root.val else root.right
+        return r
