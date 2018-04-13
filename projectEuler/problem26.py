834b0ffb15fec33e70763c6580626013dc88a264
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2018-04-13 14:55:50 -0400

    pr26 and pr27

---
+++ projectEuler/problem26.py
@@ -0,0 +1,21 @@
+# https://projecteuler.net/problem=25
+
+import itertools
+def countMax(n):
+  seen = {}
+  x = 1
+  for i in itertools.count():
+    if x in seen:
+      return i - seen[x]
+    else:
+      seen[x] = i
+      x = x * 10 % n
+
+ma = -1
+d = -1
+for i in range(2,1000):
+#   print(1/i)
+  if ma < countMax(i):
+    d = i
+    ma = countMax(i)
+print(d,ma)
---
+++ projectEuler/problem27.py
@@ -0,0 +1,34 @@
+def isPrime(n):
+    if n==2 or n==3: return True
+    if n%2==0 or n<2: return False
+    for i in range(3,int(n**0.5)+1,2):   # only odd numbers
+        if n%i==0:
+            return False
+    return True
+
+primes = {1:1}
+for i in range(2,10000):
+  if isPrime(i):
+    primes[i] = 1
+
+def f(a,b):
+  i = 0
+  while i*i + a*i + b in primes:
+    #     print(i*i + a*i + b)
+    i += 1
+  return i
+
+print(f(-79, 1601))
+
+ma = 0
+A = 0
+B = 0
+for a in range(-1001,1000):
+  for b in range(-1002,1001):
+    k = f(a,b)
+    if ma < k:
+      ma = k
+      A = a
+      B = b
+
+print(ma, A, B)
